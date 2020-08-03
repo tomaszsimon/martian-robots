567bc563142fc0997d834924cb9d082824a2ecc5
Tomasz Simon <tomasz.simon@kcom.com>
2020-08-03 12:33:14 +0100

    Read input from file

--- src/main/java/com/exercise/Application.java
+++ src/main/java/com/exercise/Application.java
@@ -1,12 +1,47 @@
 package com.exercise;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
 public class Application {
 
-    public static void main(String... args) {
-        MarsPlanet marsPlanet = new MarsPlanet(5, 3);
-        MarsRobot marsRobot = new MarsRobot(1, 1, "E", "CREATED");
-        RobotActions robotActions = new RobotActions();
-        RobotExecutor robotExecutor = new RobotExecutor(marsPlanet, robotActions);
-        robotExecutor.executeInstructions(marsRobot, "RFRFRFRF");
+    public static void main(String... args) throws IOException {
+        try (Stream<String> stream = Files.lines(Path.of(args[0]))) {
+            Iterator<String> iterator = stream.iterator();
+            if (iterator.hasNext()) {
+                MarsPlanet marsPlanet = createMarsPlanet(iterator.next());
+                RobotActions robotActions = new RobotActions();
+                RobotExecutor robotExecutor = new RobotExecutor(marsPlanet, robotActions);
+                while (iterator.hasNext()) {
+                    String robotPosition = iterator.next();
+                    MarsRobot marsRobot = createRobot(robotPosition);
+
+                    String instructions = iterator.next();
+                    robotExecutor.executeInstructions(marsRobot, instructions);
+
+                    String emptyLine = iterator.next();
+
+                    System.out.println(marsRobot);
+                }
+            }
+        }
+    }
+
+    private static MarsRobot createRobot(String robotPosition) {
+        String[] robotPositionArray = robotPosition.split(" ");
+        int x = Integer.parseInt(robotPositionArray[0]);
+        int y = Integer.parseInt(robotPositionArray[1]);
+        String orientation = robotPositionArray[2];
+        return new MarsRobot(x, y, orientation, "CREATED");
+    }
+
+    private static MarsPlanet createMarsPlanet(String marsPlanetCoordinates) {
+        String[] marsPlanetCoordinatesArray = marsPlanetCoordinates.split(" ");
+        int x = Integer.parseInt(marsPlanetCoordinatesArray[0]);
+        int y = Integer.parseInt(marsPlanetCoordinatesArray[1]);
+        return new MarsPlanet(x, y);
     }
 }
--- src/main/java/com/exercise/MarsRobot.java
+++ src/main/java/com/exercise/MarsRobot.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import static java.lang.String.format;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,4 +14,9 @@ public class MarsRobot {
     private int y;
     private String orientation;
     private String status;
+
+    @Override
+    public String toString() {
+        return format("%d %d %s %s", x, y, orientation, status.equals("LOST") ? status : "");
+    }
 }
